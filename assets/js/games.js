26572750188bda37027d1ee06629579e7a0d9b78
maktub82 <gallardo91_@hotmail.com>
2019-07-10 01:17:36 +0200

    Change source for games
    Change images
    
    Get response from XML
    
    Update to tiles
    
    Change href to open new window

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,23 +1,60 @@
-function printGames(file) {
-    var request = new XMLHttpRequest();
-    request.open("GET", file, false);
-    request.send(null)
-    var games = JSON.parse(request.responseText);
-    games.sort(function(a, b){return a.tier - b.tier});
+function printGames(games) {
+    games.sort(function (a, b) { return a.rank - b.rank });
     let items = "";
     for (let i = 0; i < games.length; i++) {
-        items += `<div class="card game-card">
-                <img src="${games[i].image}" alt="${games[i].name}">
-                <div class="card-container">
-                    <h4><b>${games[i].name}</b></h4>
-                    <div class="card-buttons">
-                        <a target="_blank" rel="noopener noreferrer" href="${games[i].review}"><img class="card-button" src='/assets/images/css/yt.svg'></a>
-                        <a target="_blank" rel="noopener noreferrer" href="${games[i].bgg}"><img class="card-button" src='/assets/images/css/bgg.svg'></a>
-                        <a target="_blank" rel="noopener noreferrer" style="display:${games[i].buy ? 'inline' : 'none'}" href="${games[i].buy}"><img class="card-button" src='/assets/images/css/buy.svg'></a>
-                    </div>
-                </div>
-            </div>`;
+        items += `<a target="_blank" 
+                     href="https://boardgamegeek.com/boardgame/${games[i].gameId}" 
+                     class="thumb thumb-img" 
+                     style="background-image: url(${games[i].thumbnail})">
+                  </a>`;
     }
 
-    document.write(`<div class="game-list">${items}</div>`)
+    document.write(`<div class="masonry">${items}</div><br/>`)
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
+}
+
+function getGames(retries) {
+    try {
+        var request = new XMLHttpRequest();
+        const username = "ikeinyyo";
+        file = `https://www.boardgamegeek.com/xmlapi2/collection?username=${username}&stats=1`
+        request.open("GET", file, false);
+        request.send(null)
+        return parseGames(request.responseXML);
+    }
+    catch
+    {
+        if (retries > 4) return []
+        console.log("error" + retries)
+        sleep(2000);
+        return getGames(retries ? retries + 1 : 1);
+    }
+}
+
+
+function parseGames(responseXML) {
+    const games = []
+    const items = responseXML.getElementsByTagName("items")[0].children;
+    for (var i = 0; i < items.length; i++) {
+        games.push({
+            image: items[i].getElementsByTagName("image")[0].textContent,
+            thumbnail: items[i].getElementsByTagName("thumbnail")[0].textContent,
+            name: items[i].getElementsByTagName("name")[0].textContent,
+            gameId: items[i].getAttribute("objectid"),
+            rank: items[i].getElementsByTagName("stats")[0].getElementsByTagName("rating")[0].getElementsByTagName("ranks")[0].children[0].getAttribute("value"),
+            owned: items[i].getElementsByTagName("status")[0].getAttribute("own") == "1",
+            wantToBuy: items[i].getElementsByTagName("status")[0].getAttribute("wanttobuy") == "1",
+            wantToPlay: items[i].getElementsByTagName("status")[0].getAttribute("wanttoplay") == "1",
+            wishList: items[i].getElementsByTagName("status")[0].getAttribute("wishlist") == "1",
+        });
+    }
+    return games;
 }
